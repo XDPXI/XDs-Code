67c8734e5f3339c7b4c973638e37b1ecc7c1d53e
Xavier Perri <xdpxigaming@gmail.com>
2025-05-06 15:46:24 +1000

    Removed Installer

--- update.bat
+++ update.bat
@@ -4,22 +4,5 @@ echo == Updating JavaScript dependencies (pnpm)... ==
 start /wait "" cmd /c "@echo off & pnpm update"
 start /wait "" cmd /c "@echo off & pnpm install"
 
-echo == Updating Rust dependencies (Cargo)... ==
-cd src-tauri || exit /b
-start /wait "" cmd /c "@echo off & cargo update"
-cd ..
-
-echo == Checking for .NET dependency updates... ==
-cd installer || exit /b
-
-where dotnet-outdated >nul 2>&1
-if %errorlevel% neq 0 (
-    echo Installing dotnet-outdated-tool...
-    start /wait "" cmd /c "@echo off & dotnet tool install --global dotnet-outdated-tool"
-)
-
-start /wait "" cmd /c "@echo off & dotnet outdated --upgrade"
-cd ..
-
 echo == All dependencies updated! ==
 pause
