b902f438fe5cce11c79249970c3117989be2e1e4
XDPXI <119396780+XDPXI@users.noreply.github.com>
2025-12-21 14:52:20 +1100

    0.7.1

--- installer/MainWindow.xaml.cs
+++ installer/MainWindow.xaml.cs
@@ -58,14 +58,14 @@ namespace Installer
 
         private const string AppExeName = "xds-code.exe";
         private const string AppDisplayName = "XD's Code";
-        private const string AppVersion = "0.7.0";
+        private const string AppVersion = "0.7.1";
         private const string ShortcutName = AppDisplayName + ".lnk";
 
         private const string RegistryUninstallKey =
             @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\" + AppDisplayName;
 
         private const string DownloadUrl =
-            "https://raw.githubusercontent.com/XDPXI/XDs-Code/main/installer/releases/0.7.0.zip";
+            "https://raw.githubusercontent.com/XDPXI/XDs-Code/main/installer/releases/0.7.1.zip";
 
         #endregion
 
