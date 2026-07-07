1fc15ef88104437992eab5877cf4c962bccf2bb4
Xavier Perri <contact@xdpxi.dev>
2026-07-07 12:04:46 +1000

    Update vite chunking to split monaco editor, xterm and react

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,18 @@ const host = null;
 export default defineConfig(async () => ({
   plugins: [react()],
 
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("monaco-editor") || id.includes("@monaco-editor")) return "monaco-editor";
+          if (id.includes("xterm")) return "xterm";
+          if (id.includes("react-dom") || id.includes("react/")) return "react";
+        },
+      },
+    },
+  },
+
   clearScreen: false,
   server: {
     port: 6382,
